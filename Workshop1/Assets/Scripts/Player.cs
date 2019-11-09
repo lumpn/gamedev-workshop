8f66082e0ddcbece265bfd74e460df4aedf12517
LumpN <github@lumpn.de>
2019-11-09 18:39:49 +0800

    Basic project setup

---
+++ Workshop1/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private float speed; 
+
+    void Update()
+    {
+        var deltaX = Input.GetAxis("Horizontal");
+        var deltaZ = Input.GetAxis("Vertical");
+        var forward = new Vector3(deltaX, 0, deltaZ);
+
+        if (forward.sqrMagnitude > 0.01f)
+        {
+            transform.localRotation = Quaternion.LookRotation(forward, Vector3.up);
+        }
+
+        controller.SimpleMove(forward * speed);
+    }
+}
