47395097407d825112116223b78bef4996d94b3a
Jonas Boetel <lumpn@users.noreply.github.com>
2020-01-04 21:52:21 +0800

    Added demo project

---
+++ ContinuousIntegration/Assets/Scripts/Editor/TestFormatting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+        [Test]
+        public void TestLineEndings()
+        {
+            var guids = AssetDatabase.FindAssets("t:script");
+            foreach (var guid in guids)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+            }
+        }
+    }
+}
