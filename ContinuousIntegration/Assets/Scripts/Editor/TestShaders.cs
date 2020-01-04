508a17927de099adf3fa26c0ad99127281603d11
Jonas Boetel <lumpn@users.noreply.github.com>
2020-01-05 00:58:54 +0800

    Improving demo

---
+++ ContinuousIntegration/Assets/Scripts/Editor/TestShaders.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestShaders
+    {
+        [Test]
+        public void ShaderHasErrors()
+        {
+var infos = ShaderUtil.GetAllShaderInfo();
+
+        }
+    }
+}
