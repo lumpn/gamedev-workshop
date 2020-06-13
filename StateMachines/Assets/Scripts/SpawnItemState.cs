9b1c7004a691e353454b177325e1b7dc775205ec
lumpn <lumpn@users.noreply.github.com>
2020-06-13 22:17:35 +0800

    Spawn item demo

--- StateMachines/Assets/Scripts/SpawnItemState.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public sealed class SpawnItemState : StateMachineBehaviour
-{
-    [SerializeField] public GameObject prefab;
-    [SerializeField] public Transform attachPoint;
-}
