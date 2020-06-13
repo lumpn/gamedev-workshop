637648e12d17099bbe8e5aee61c95ec9b75b361a
Jonas Boetel <lumpn@users.noreply.github.com>
2020-06-13 15:38:28 +0800

    Exploring StateMachineBehaviour

---
+++ StateMachines/Assets/Scripts/PlayableDirectorState.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public sealed class PlayableDirectorState : StateMachineBehaviour
+{
+    public enum Action
+    {
+        None,
+        Play,
+        Stop,
+        Pause,
+        Resume,
+    }
+
+    [SerializeField] private PlayableAsset playableAsset;
+    [SerializeField] private Action onEnter, onExit;
+
+    private Playable playable;
+
+    void OnEnable()
+    {
+        var graph = PlayableGraph.Create("JonasWasHere");
+        var owner = new GameObject("Owner");
+        playable = playableAsset.CreatePlayable(graph, owner);
+    }
+
+    void OnStateEnter()
+    {
+        HandleAction(onEnter);
+    }
+
+    void OnStateExit()
+    {
+        HandleAction(onExit);
+    }
+
+    private void HandleAction(Action action)
+    {
+        switch (action)
+        {
+            case Action.None: break;
+            case Action.Play: playable.Play(); break;
+            case Action.Stop: playable.Pause(); break;
+            case Action.Pause: playable.Pause(); break;
+            case Action.Resume: playable.Play(); break;
+            default: Debug.LogErrorFormat("Unhandled action {0}", action); break;
+        }
+    }
+}
---
+++ StateMachines/Assets/Scripts/SpawnItemState.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class SpawnItemState : StateMachineBehaviour
+{
+    [SerializeField] public GameObject prefab;
+    [SerializeField] public Transform attachPoint;
+}
