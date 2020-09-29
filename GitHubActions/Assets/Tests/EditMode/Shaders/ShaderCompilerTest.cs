0093aa91377c24101fd2df61182eedbdf30d38c3
lumpn <lumpn@users.noreply.github.com>
2020-09-29 14:51:59 +0000

    Added GitHub Actions project

---
+++ GitHubActions/Assets/Tests/EditMode/Shaders/ShaderCompilerTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using UnityEditor;
+
+namespace Rara.Tests
+{
+    public sealed class ShaderCompilerTest
+    {
+        [Test]
+        public void TestShaderHasErrors()
+        {
+            var infos = ShaderUtil.GetAllShaderInfo();
+            foreach (var info in infos)
+            {
+                Assert.IsFalse(info.hasErrors, "Shader '{0}' has errors.", info.name);
+            }
+        }
+    }
+}
